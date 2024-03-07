443a724cc5c3de6711f51eff0e77534b2876122f
Vinaykumargm <162595523+Vinaykumargm@users.noreply.github.com>
2024-03-07 20:19:45 +0530

    Create mov.py

---
+++ mov.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 12:22:56 2023
+
+@author: Admin
+"""
+
+movie={
+     "Leo":{"language":"Tamil","Genre":"Action-FamilyDrama","Duration":"3hrs","Cast":"Vijay,Trisha,SanjayDuttr","Director":"Lokesh kanagaraj"},
+     "Ghost":{"language":"kannada","Genre":"Action-Thriller","Duration":"2hrs.30min","Cast":"Shivarajkumar,Anupam-Kher","Director":"Sreene"},
+     "Vikrant-rona":{"language":"kannada","Genre":"Scify","Duration":"3hrs.23min","Cast":"Badshah-Kiccha Sudeep,milana-nagaraj","Director":"NirupBhandari"},
+     "Baby":{"language":"Telugu","Genre":"Romance/Drama","Duration":"2hrs.55min","Cast":"Vaishnvai Chaitanya,Anand Devarakonda","Director":"Sai Rajesh neelam"},
+     "SSE(side-B)":{"language":"Kannada","Genre":"Drama,Romantic","Duration":"3hrs","Cast":"Simple_star-Rakshit sheety,Rukmini vasanth","Director":"Hemanth Rao"}
+     }
