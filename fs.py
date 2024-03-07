96843f973b7c28e075d1386175bfd4809e3946ff
Vinaykumargm <162595523+Vinaykumargm@users.noreply.github.com>
2024-03-07 20:16:28 +0530

    Create fs.py

---
+++ fs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 09:30:37 2023
+
+@author: Admin
+"""
+
+di={
+    "mukta":"998950",
+    "vinay":"998951",
+    "nandu":"998952",
+    "akhil":"998953"
+    }
